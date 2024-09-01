2c97a1d131ae7dbcebedbf45cb9059d82afb9745
Netetra <me@netetra.dev>
2024-09-01 10:18:37 +0900

    feat: read file and generate ast and print ast

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{env, fs, process};
+
+use parser::primo_parser;
+mod ast;
+mod parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <file-path>", args[0]);
+        process::exit(1);
+    }
+
+    let file_path = &args[1];
+    let input = match fs::read_to_string(file_path) {
+        Ok(contents) => contents,
+        Err(err) => {
+            eprintln!("Error reading file: {}", err);
+            process::exit(1);
+        }
+    };
+
+    let ast = primo_parser::program(&input).unwrap();
+
+    println!("AST: {:?}", ast);
 }
